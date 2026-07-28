585eaaa2d25d759643b0cdda2f4734c9c887b364
Cato Yeung <me@catoyeung.com>
2026-07-28 15:44:02 +0800

    Compress PWA icons

--- web/service_worker.js
+++ web/service_worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cy-complete-v1';
+const CACHE_NAME = 'cy-complete-v2';
 const APP_SHELL = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
